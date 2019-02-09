caefe35be6cfd6235ff090db855fc299af184db3
C-Lo 12 <chris_imk@hotmail.com>
2019-02-09 17:51:25 -0600

    Insertar al inicio, final. Eliminar al inicio, final

---
+++ programacion3_2019/listaEnlazada - copia/Nodo.java
@@ -0,0 +1,25 @@
+public class Nodo{
+    private Object dato;
+    private Nodo sig;
+
+    public Nodo(Object dato){
+        this.dato = dato;
+        this.sig = null;
+    }
+
+    public Object getDato(){
+        return dato;
+    }
+
+    public void setDato(Object dato){
+        this.dato=dato;
+    }
+
+    public Nodo getSig(){
+        return this.sig;
+    }
+
+    public void setSig(Nodo sig){
+        this.sig=sig;
+    }
+}
--- programacion3_2019/listaEnlazada/src/ListaEnlazada.java
+++ programacion3_2019/listaEnlazada/src/ListaEnlazada.java
@@ -3,9 +3,11 @@ import java.util.Date;
 public class ListaEnlazada{
 
     private Nodo inicio;
+    private Nodo ultimo;
 
     public ListaEnlazada(){
         inicio=null;
+        ultimo=null;
     }
 
     public boolean estaVacia(){
@@ -18,16 +20,50 @@ public class ListaEnlazada{
         //2. revisar si la lista no esta vacia
         if(!estaVacia())
             nuevo.setSig(inicio);
+        else
+            ultimo = nuevo;
         //3. el nuevo nodo es el inicial de la lista
         inicio = nuevo;
+    }
+    
+    public void agregarFinal(Object dato){
+        //escenario 1 la lista esta vacia
+        if(estaVacia())
+            agregarInicio(dato);
+        else{
+           //1. crear Nodo
+            Nodo nuevo = new Nodo(dato); 
+            //2. el siguiente del ultimo apunta al nuevo
+            ultimo.setSig(nuevo);
+            //3. el ultimo apunta al nuevo
+            ultimo = nuevo;
+        }
     }
 
     public void eliminarInicio(){
         //1.cambiar inicio al segundo nodo
         if(!estaVacia())
             inicio = inicio.getSig();
+        if(inicio == null)
+            ultimo = inicio;
     }
 
+    public void eliminarFinal(){
+        //1. Escenario cuando la lista tiene 1 nodo
+        if(inicio==ultimo && inicio!=null){
+            inicio=ultimo=null;
+        }else if(!estaVacia()){
+            //2. Escenario donde hay más de 1 nodo
+            //2.1 encontrar penultimo
+            Nodo penultimo = devolverPenultimo();
+            //2.2 ultimo debe apuntar al penultimo
+            this.ultimo=penultimo;
+            //2.3 el siguiente del ultimo debe ser nulo
+            ultimo.setSig(null);
+        }
+
+    }
+    
     public void recorrer(){
         Nodo temp = inicio;
         while(temp!=null){
@@ -39,16 +75,28 @@ public class ListaEnlazada{
         }
         System.out.println("");
     }
+        
+    public Nodo devolverPenultimo(){
+        Nodo temp = inicio;
+        while(temp.getSig()!=ultimo){
+            //2. cambiar el apuntador al siguiente nodo
+            temp=temp.getSig();
+        }
+        return temp;
+    }
+    
+    
 
     public static void main(String[] args){
         ListaEnlazada lista1 = new ListaEnlazada();
-
-        lista1.agregarInicio("OLA KE ASE");
-        lista1.agregarInicio(new Integer(12));
-        lista1.agregarInicio(new Date());
-        lista1.recorrer();
-        lista1.eliminarInicio();
-        lista1.recorrer();
+        lista1.agregarFinal(50);
+        lista1.agregarFinal(35);
+        lista1.agregarFinal(5);
+        //lista1.recorrer();
+        lista1.eliminarFinal();
+        lista1.eliminarFinal();
+        lista1.eliminarFinal();
+        //lista1.recorrer();
     }
 }
 
