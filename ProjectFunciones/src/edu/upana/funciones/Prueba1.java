b4ed68d723f27e139c2de52de02de49a4298b378
C-Lo 12 <chris_imk@hotmail.com>
2019-02-21 19:31:05 -0600

    Merge origin/master
    Conflicts:
    	.gitignore

--- ProjectFunciones/src/edu/upana/funciones/Prueba1.java
+++ ProjectFunciones/src/edu/upana/funciones/Prueba1.java
@@ -16,15 +16,22 @@ public class Prueba1 {
     }
      
     public static void main(String []args){
-        String nombre;
-        int edad;
+        String nombre,nombre2;
+        int edad,edad2;
         
         //1. asignar valores
         nombre = "Kely";
         edad = 19;
         //2. mostrar esta informacion en pantalla
-        pintarNombre(nombre,edad);    
+        pintarNombre(nombre,edad); 
+        nombre = "Kely Quevedo";
         edad +=10;
-        pintarNombre(nombre,edad);  
+        pintarNombre(nombre,edad); 
+        
+        nombre2="Jose Hernandez";
+        edad2=38;
+        pintarNombre(nombre2, edad2);
+        
+        pintarNombre("Bryam Barrientos",21);
     }
 }
